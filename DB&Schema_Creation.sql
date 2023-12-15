aa7337a30f88e7f7c8fcb9e2840cdcf79550aa38
Prema Prabu <prema@Premas-Air.hsd1.ca.comcast.net>
2023-12-14 21:53:07 -0800

    added

---
+++ DB&Schema_Creation.sql
@@ -0,0 +1,14 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2758
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 ----------------MRL_DEV_DB CREATION----------------\
+CREATE OR REPLACE DATABASE  MTL_DEV_DB;\
+\
+------------------SCHEMA CREATION -----------------\
+CREATE OR REPLACE SCHEMA MTL_DEV_DB.MTL_GOLD;\
+CREATE OR REPLACE SCHEMA MTL_DEV_DB.MTL_BRONZE;\
+CREATE OR REPLACE SCHEMA MTL_DEV_DB.MTL_SILVER;}
